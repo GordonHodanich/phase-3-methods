b669f0cdaaf4c3bf801b5a75452b9429199c8977
Gordon Hodanich <ghodanich@gmail.com>
2021-08-03 11:01:06 -0400

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(number1, number2)
+  number1 + number2
+end
+
+def halve(number)
+  return nil unless number.class == Integer
+  number/2
+end
